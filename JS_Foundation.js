a3b1388437b9f00852339580f015228be6af846b
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 15:27:03 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -130,3 +130,6 @@ console.log("02">1) //comparison chanes the data type
 
 console.log("2"=== 2) // === will check condition as well as the datatype
  */
+
+
+// console.log("************ Memory *****************")
