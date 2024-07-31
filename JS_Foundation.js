a0013e7658181ab31388b02fdf11d8e2eee951ad
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 12:44:42 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -79,9 +79,12 @@ console.log(1 + "2");
 console.log(1 + 2);
 console.log("1" + 2 + 2); // after adding a string if we put a number still it behave like string
 console.log(1 + 2 + "2"); // before adding a string if we put a number it will calculate the number then concat to the string
-*/
+
 
 let gamecounter = 100
-gamecounter++
+gamecounter++ //postfix
+++gamecounter //prefix - increament before 
 
 console.log(gamecounter);
+
+*/
