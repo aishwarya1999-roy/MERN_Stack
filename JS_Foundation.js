0160b6a019590c5a1d6011630c2b30ea05149818
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 14:39:47 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -125,7 +125,7 @@ console.log(gamecounter);
 // ------------- Datatype Comparison  ----------------
 /*
 
-console.log(" ************ Datatype Comparison *****************")
+console.log("************ Datatype Comparison *****************")
 console.log(2>1)
 console.log(2>=1)
 console.log("2">1)
