76567d667d7a925783fe7d70f759fde445b1e920
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 12:21:27 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -43,5 +43,5 @@ let score = "33"
 //console.log(typeof valueinNumber)
 //console.log(typeof Number(score))
 
-let score2 = "33an"
+let score2 = True
 console.log(Number(score2)) //NaN - not a number
