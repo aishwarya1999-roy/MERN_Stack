b39706b69419fa82446090d5e88e97fcb16737cb
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 12:01:35 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -1,1 +1,28 @@
-console.log("Hello") //print
+console.log("~~ Hey there, This is all about JS foundation ~~") //print
+
+/* console.log(), const, var, let */
+
+const EmpID = 40 //constant declaration, can't change the value
+let EmpMail = "aish@gmail.com" //variable declaration - scope in var
+var contact = 123456 //variable declaration - no scope in var - never use var
+city = "Kolkata" //variable declaration - we can declare without let/var but it is not recomemded. 
+let account_state;
+/*  Prefer not to use var as there are issue with block scope & funcational scope*/
+console.log(EmpID, EmpMail, contact, city)
+console.table([EmpID, EmpMail, contact, city, account_state])
+
+/* Datatype */
+
+"use strict"; //treat all JS code as newer version - as of now it takes it default
+
+//alert("Hello") // works on browser
+
+let name = "Aish" //string
+let age = 25 //number 2 to the power 53
+let islogginin = false //boolean
+
+// bigint - bigger than number
+// null = standalone value
+// undefinded = value not defined 
+
+
