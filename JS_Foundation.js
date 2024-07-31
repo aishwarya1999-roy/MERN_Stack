a763567c1655f769ede432d53fab9d62476fce62
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 13:13:35 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -91,10 +91,10 @@ console.log(gamecounter);
 
 
 // ------------- Datatype Comparison  ----------------
-
-// console.log(2>1)
-// console.log(2>=1)
-// console.log("2">1)
-// console.log("02">1) //comparison chanes the data type
+/* */
+console.log(2>1)
+console.log(2>=1)
+console.log("2">1)
+console.log("02">1) //comparison chanes the data type
 
 console.log("2"=== 2) // === will check condition as well as the datatype
