0374b6c6e4b286d7a317046838c5449e14ab9ffb
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-07-31 16:43:44 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -139,7 +139,7 @@ console.log("2"=== 2) // === will check condition as well as the datatype
 
 let ytname = "Aishwarya"
 let anothername = ytname
-
+anothername = "Adwe"
 console.log(anothername);
 
 
