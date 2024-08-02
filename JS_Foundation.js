ead4c1de5556ead928917859dc727a28cfe8eefc
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 10:30:03 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -192,7 +192,7 @@ console.log(gamename.substring());
 // console.log(gamename.codePointAt(8)); //return ASCII value
 // console.log(gamename.endsWith('b'));
 // console.log(gamename.includes('a'));
-console.log(gamename.lastIndexOf('a'));
+console.log(gamename.lastIndexOf('a')); // return index of last character in case of duplicate character
 
 
 
