ef0ec4fb3f7cb9377d7de822567eec4709a995f2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 13:00:42 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -204,28 +204,27 @@ console.log(gamename.split(',')); //split the string and make array
 console.log(trimstring.trimEnd()); //trim from end
 console.log(trimstring.trimStart()); //trim from front
 console.log(trimstring.trim()); //trim from both end
+*/
 
 
+// console.log(gamename.substring());
+// // substring
+// const newString = gamename.substring(2,5)
+// console.log(newString);
+// // slicing
+// const anotherString = gamename.slice(-9,5)
+// console.log(anotherString);
 
-console.log(gamename.substring());
-// substring
-const newString = gamename.substring(2,5)
-console.log(newString);
-// slicing
-const anotherString = gamename.slice(-9,5)
-console.log(anotherString);
-
-// A  i  s  h  w  a  r  y  a
-// 0  1  2  3  4  5  6  7  8
-//-9 -8 -7 -6 -5 -4 -3 -2 -1 - Slice backward index
+// // A  i  s  h  w  a  r  y  a
+// // 0  1  2  3  4  5  6  7  8
+// //-9 -8 -7 -6 -5 -4 -3 -2 -1 - Slice backward index
 
 const url = "https://aishwarya.com/aishwarya%20roy"
-console.log(url.replace('%20','-'));
+// console.log(url.replace('%20','-'));
+console.log(trimstring.trim()); 
 
-
-const s1 = "2 + 2"; // creates a string primitive
-const s2 = new String("2 + 2"); // creates a String object
-console.log(eval(s1)); // returns the number 4
-console.log(eval(s2)); // returns the string "2 + 2"
-console.log(eval(s2.valueOf())); // returns the number 4
-*/
+// const s1 = "2 + 2"; // creates a string primitive
+// const s2 = new String("2 + 2"); // creates a String object
+// console.log(eval(s1)); // returns the number 4
+// console.log(eval(s2)); // returns the string "2 + 2"
+// console.log(eval(s2.valueOf())); // returns the number 4
