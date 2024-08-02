30a9d558773e6b1b03fd91bfe9dea05bd9358379
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 13:33:03 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -244,4 +244,4 @@ console.log(balance.toExponential(2));
 
 const hundreds = 4000000
 console.log(hundreds.toLocaleString('en-IN'));
-console.log(Number.MIN_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
