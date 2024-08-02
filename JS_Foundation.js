5436809b4bca8b177ee94c87e00b61e2a6ce0520
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 12:17:05 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -199,10 +199,10 @@ console.log(gamename.substring());
 // console.log(gamename.repeat(2)); // repeat the string 
 //console.log(gamename.split(',')); //split the string and make array
 
-console.log(trimstring.trimEnd()); //trim from end
-console.log(trimstring.trimStart()); //trim from front
-console.log(trimstring.trim()); //trim from both end
-
+// console.log(trimstring.trimEnd()); //trim from end
+// console.log(trimstring.trimStart()); //trim from front
+// console.log(trimstring.trim()); //trim from both end
+console.log(trimstring.valueOf());
 
 /*
 console.log(gamename.substring());
@@ -216,11 +216,6 @@ console.log(anotherString);
 // A  i  s  h  w  a  r  y  a
 // 0  1  2  3  4  5  6  7  8
 //-9 -8 -7 -6 -5 -4 -3 -2 -1 - Slice backward index
-
-
-const trimstring = "   Aishwarya space   "
-console.log(trimstring);
-console.log(trimstring.trim());
 
 const url = "https://aishwarya.com/aishwarya%20roy"
 console.log(url.replace('%20','-'));
