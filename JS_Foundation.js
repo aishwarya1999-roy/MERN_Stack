56e5d433bb00984a088a7909482d991a5b10d276
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 12:40:18 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -223,3 +223,11 @@ const url = "https://aishwarya.com/aishwarya%20roy"
 console.log(url.replace('%20','-'));
 
 */
+const areEqualInUpperCase = (str1, str2) =>
+    str1.toUpperCase() === str2.toUpperCase();
+  const areEqualInLowerCase = (str1, str2) =>
+    str1.toLowerCase() === str2.toLowerCase();
+  
+  areEqualInUpperCase("ß", "ss"); // true; should be false
+  areEqualInLowerCase("ı", "I"); // false; should be true
+  
