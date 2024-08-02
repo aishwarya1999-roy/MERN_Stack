6b009ff1b204f7d6039473068fbe22283138550d
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 12:49:17 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -222,9 +222,10 @@ console.log(anotherString);
 const url = "https://aishwarya.com/aishwarya%20roy"
 console.log(url.replace('%20','-'));
 
-*/
+
 const s1 = "2 + 2"; // creates a string primitive
 const s2 = new String("2 + 2"); // creates a String object
 console.log(eval(s1)); // returns the number 4
 console.log(eval(s2)); // returns the string "2 + 2"
 console.log(eval(s2.valueOf())); // returns the number 4
+*/
