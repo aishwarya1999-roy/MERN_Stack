920949d7a455387e20c2e83525914518a3d4a052
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-02 12:20:18 +0530

    Auto-commit

--- JS_Foundation.js
+++ JS_Foundation.js
@@ -172,37 +172,37 @@ console.log(user_two);
 
 // console.log("************ String *****************")
 
-// const myname = "aish"
-// const repcount = 13
-//console.log(myname + repcount + "dew"); //outdated
+const myname = "aish"
+const repcount = 13
+console.log(myname + repcount + "dew"); //outdated
 
-//console.log(`Hello my name is ${myname} and my repo count is ${repcount}`);
+console.log(`Hello my name is ${myname} and my repo count is ${repcount}`);
 
 const gamename = new String('AishwaryA') //creating string as a object to use as a constructor
 const trimstring = "   Aishwarya space   "
-// console.log(gamename[0]);
+console.log(gamename[0]);
 console.log(gamename.valueOf()); //return premitive value of the object
-//console.log(gamename.substring(2,4)); //slice into substring
-// console.log(gamename.__proto__);
-// console.log(gamename.length); //len of string
-// console.log(gamename.toUpperCase());
-// console.log(gamename.toLowerCase());
-// console.log(gamename.charAt(2));
-// console.log(gamename[2]);
-//console.log(gamename.indexOf('is')); // return index of first character in case of duplicate character
-//console.log(gamename.search('is'))// return index of first substring
-// console.log(gamename.charCodeAt(0)); //return ASCII value
-// console.log(gamename.codePointAt(8)); //return ASCII value
-//console.log(gamename.startsWith('g'));
-// console.log(gamename.endsWith('b'));
-// console.log(gamename.includes('a'));
-// console.log(gamename.lastIndexOf('a')); // return index of last character in case of duplicate character
-// console.log(gamename.repeat(2)); // repeat the string 
-//console.log(gamename.split(',')); //split the string and make array
+console.log(gamename.substring(2,4)); //slice into substring
+console.log(gamename.__proto__);
+console.log(gamename.length); //len of string
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(2));
+console.log(gamename[2]);
+console.log(gamename.indexOf('is')); // return index of first character in case of duplicate character
+console.log(gamename.search('is'))// return index of first substring
+console.log(gamename.charCodeAt(0)); //return ASCII value
+console.log(gamename.codePointAt(8)); //return ASCII value
+console.log(gamename.startsWith('g'));
+console.log(gamename.endsWith('b'));
+console.log(gamename.includes('a'));
+console.log(gamename.lastIndexOf('a')); // return index of last character in case of duplicate character
+console.log(gamename.repeat(2)); // repeat the string 
+console.log(gamename.split(',')); //split the string and make array
 
-// console.log(trimstring.trimEnd()); //trim from end
-// console.log(trimstring.trimStart()); //trim from front
-// console.log(trimstring.trim()); //trim from both end
+console.log(trimstring.trimEnd()); //trim from end
+console.log(trimstring.trimStart()); //trim from front
+console.log(trimstring.trim()); //trim from both end
 
 
 /*
