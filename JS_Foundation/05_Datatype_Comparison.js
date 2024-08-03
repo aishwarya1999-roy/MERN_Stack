928a6e1b19a496e306e9c5dc14c11e96094c42a8
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-03 21:47:59 +0530

    Auto-commit

---
+++ JS_Foundation/05_Datatype_Comparison.js
@@ -0,0 +1,8 @@
+console.log("************ Datatype Comparison *****************")
+
+console.log(2>1)
+console.log(2>=1)
+console.log("2">1)
+console.log("02">1) //comparison chanes the data type
+
+console.log("2"=== 2) // === will check condition as well as the datatype
