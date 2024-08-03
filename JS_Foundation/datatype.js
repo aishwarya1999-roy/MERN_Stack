b12e6342b87ab9dd712dea02a0ad8c2fd3a84c0e
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-03 21:43:13 +0530

    Auto-commit

---
+++ JS_Foundation/datatype.js
@@ -0,0 +1,52 @@
+console.log("************ Datatype *****************")
+
+//alert("Hello") // works on browser
+
+// Primitve & Non primitive data type
+// Primitive - 7type : string, number, boolean, null, undefined, symbol, bigint
+// Refernce Type/ Non premitive - 
+//Array, objects, Functions
+
+
+let name = "Aish" //string declarion
+let age = 25 //number declarion, number range = 2 to the power 53
+let islogginin = false //boolean declarion
+
+//symbol = unique
+const id = Symbol('123')
+const another_id = Symbol('123')
+console.log(id === another_id; //false 
+
+// bigint - bigger than number
+const bignum = 41656598456n //bigint declarion
+
+// null = standalone value / blank / empty
+// undefinded = value not defined 
+
+console.log(typeof null) //get the type of the variable 
+
+//array creation
+const heros = ["iron", "hjii", 'idji']
+console.log(heros);
+
+// object creation = object is like dictionary in python
+let myobj = {             
+    first_name : "Aishw",
+    my_age: 23,
+}
+console.log(myobj);
+
+
+// function creaton
+const myfunction = function(){
+    console.log("hello")
+    return "Aishwarya"
+}
+console.log(myfunction())
+
+// function creaton another way
+function functionanotherWay(){
+    console.log("hello")
+    return "Aishwarya"
+}
+console.log(functionanotherWay())
