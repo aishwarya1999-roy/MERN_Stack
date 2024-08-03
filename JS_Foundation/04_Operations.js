38b435b0f31430b056bfbfd6b990082b9476cf36
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-03 21:45:57 +0530

    Auto-commit

---
+++ JS_Foundation/04_Operations.js
@@ -0,0 +1,27 @@
+// console.log("************ Operations *****************")
+
+let val = 3
+let negva = -val
+//console.log(negva);
+
+console.log(3+3)
+console.log(3-3)
+console.log(3*3)
+console.log(3**3)
+console.log(3/3)
+console.log(3%3)
+
+let str1 = "Hello"
+let str2 = " Aish"
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2);
+console.log("1" + 2 + 2); // after adding a string if we put a number still it behave like string
+console.log(1 + 2 + "2"); // before adding a string if we put a number it will calculate the number then concat to the string
+
+
+let gamecounter = 100
+gamecounter++ //postfix - increament after
+++gamecounter //prefix - increament before 
+console.log(gamecounter);
