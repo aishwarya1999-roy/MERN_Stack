4390f46ea1905c5b3b9ff8c0d6dc3a18b78d5bd2
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-03 21:50:38 +0530

    Auto-commit

---
+++ JS_Foundation/06_Memory.js
@@ -0,0 +1,27 @@
+
+console.log("************ Memory - Stack/Heap *****************")
+
+//Stack (used for primitive type - number, string, boolean etc) & Heap (used for non primitive type - functions/array/objects)
+
+//stack - take a copy then change, nothing will change in original 
+let ytname = "Aishwarya"
+let anothername = ytname
+anothername = "Adwe"
+console.log(ytname);
+console.log(anothername);
+
+
+//Heap  - take refernce and update - update in both
+let user_one = {
+    email : "ajhd@gmail.com",
+    upi : "dhu@oksbi"
+}
+
+let user_two = user_one
+console.log(user_two);
+
+user_two.email = "aish@gmail.com"
+
+console.log(user_one);
+console.log(user_two);
+// here email value will change for user one & user two both
