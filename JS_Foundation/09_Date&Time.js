9d4cf8692f92f8efb712c47e7d7f458c4e50500c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-03 22:10:28 +0530

    Auto-commit

---
+++ JS_Foundation/09_Date&Time.js
@@ -0,0 +1,22 @@
+// console.log("************ Date & time  *****************")
+/**  
+let mydate = new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toTimeString());
+console.log(mydate.getHours());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+
+let my_created_date = new Date(2024, 7, 5) //let my_created_date = new Date(2024, 7, 5,5, 3)
+let another_date = new Date(2024, 7, 5,5, 3)
+console.log(my_created_date.toLocaleString());
+let myTimestamp = Date.now()
+console.log(myTimestamp);
+console.log(Math.floor(Date.now()/1000));
+console.log(`Date is ${mydate.getDay()}`);
+console.log(mydate.toLocaleString('default', {weekday: "long"}))
+*/
