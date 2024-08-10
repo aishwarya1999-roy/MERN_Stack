3badfe149e836b12934e8e5c9e19f023b9bb01ea
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-10 11:01:09 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -271,5 +271,39 @@ console.log(mydate.toLocaleString('default', {weekday: "long"}))
 
 
 
-// console.log("************ Date & time  *****************")
+// console.log("************ Array *****************")
+
+//shallow copy of an object is a copy whose properties share the same referance  - so if we change anything in source or copy file, both the file changes
+//deep copy of an object is a copy whose properties don't share the same referance  - not disturing the source changing anything to copy and vice versa
+
+const arr = [1, 4, 6, 7, 8]
+//console.log(arr[1]);
+
+// arr.push(10) // add in the last
+//console.log(arr);
+// arr.pop() //pop from last
+// console.log(arr);
+
+// arr.unshift(111) //add in begining
+// arr.unshift(111) //remove from begining
+// console.log(arr);
+// console.log(arr.includes(8));
+// console.log(arr.indexOf(8));
+// console.log(arr.indexOf(8));
+
+//const new_ary = arr.join() //join a list to string
+// console.log(new_ary);
+
+//slice
+console.log("A ", arr); 
+const new_slice = arr.slice(1,3) //Slice - return only the sliced part of the list, it return like (start_index, end_index), it doesn't affect  anything to the original array
+console.log(new_slice);
+
+console.log("B ", arr);
+const new_splice = arr.splice(1,3) //Splice - it separates the splice part form the array, and return like (start_index, how many numbers you wanna return), and the original array remain with the remaning elements, so it changes the original array
+console.log(new_splice);
+
+console.log("C ", arr);
+
+
 
