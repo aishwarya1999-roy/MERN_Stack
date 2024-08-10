15eb77e61f665a1cfbe2c14bce58da7ecf16d1e4
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-10 11:16:07 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -319,7 +319,7 @@ const nested_array = [1,2,3,[4,5],6,[7,8,[9,10]]]
 
 
 console.log(Array.isArray("Aishwarya"));
-console.log(Array.isArray("Aishwarya"));
+console.log(Array.from("Aishwarya"));
 
 
 
