36c8cc7b72c7d01be486cc87b54c88b18818b757
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-08-10 11:07:41 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -309,8 +309,8 @@ const mar_hero = ["he", "she" , "kk"]
 const DC_hero = ["hwe", "ewe" , "wae"]
 
 //mar_hero.push(DC_hero) //array under array
-
-console.log(mar_hero.concat(DC_hero));
+pp = mar_hero.concat(DC_hero)
+console.log(pp);
 
 
 
