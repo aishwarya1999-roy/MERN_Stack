17cf1a50334287875549885b452ce0b5884467b3
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-05 17:08:21 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -388,6 +388,6 @@ const regularUser = {
 const obj1 = { 1:'a', 2:'b'}
 const obj2 = { 3:'c', 4:'d'}
 // const obj3 = {obj1,obj2}
-const obj3 = Object.assign({}, obj1, obj2) //assign(target , source
+const obj3 = Object.assign({}, obj1, obj2) //assign(target , source)
 console.log(obj3);
 
