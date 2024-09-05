ba9d800513ab87e2519c17a53f8f0317ef13a3f8
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-05 16:38:43 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/11_Objects.js
+++ JS_Foundation/Basics_2.0/11_Objects.js
@@ -1,22 +1,32 @@
+
 // console.log("************ Objects *****************")
 
 //Object.create // constructor way
 //object literals
 
-const myst = Symbol("Key1")
+//const myst = Symbol("Key1")
 
 const js_user = {
     "name" : "Aish",
     age : 25,
     City : "Kolkata",
-    [myst] : "MyKey1",
+    myst : "MyKey1",
     isloggin : false
 }
-Object.freeze(js_user)
+// Object.freeze(js_user)
 // console.log(js_user.name);
 // console.log(js_user["name"]);
 // console.log(js_user["age"]);
 // console.log(js_user[myst]);
-js_user.City = "howrah"
+// js_user.City = "howrah"
+// console.log(js_user);
 
-console.log(js_user);
+js_user.greeting = function(){
+    console.log("Hello, Good Morning");
+}
+console.log(js_user.greeting());
+
+js_user.greetingto = function(){
+    console.log(`Hello, Good Morning ${this.name}`);
+}
+console.log(js_user.greetingto());
