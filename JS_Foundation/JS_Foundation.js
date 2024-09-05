dc4cfc8ea28188b8da81b1e9a28270ced71946ee
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-05 17:05:43 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -383,7 +383,7 @@ const regularUser = {
     address : "ABC, road, 71110"
 }
 
-console.log(regularUser.fullname?.usersname.firstname); // ? is used for error handeling, if the fullname object is not there then it won't throw error, it just give undefined
+//console.log(regularUser.fullname?.usersname.firstname); // ? is used for error handeling, if the fullname object is not there then it won't throw error, it just give undefined
 
 const obj1 = { 1:'a', 2:'b'}
 const obj2 = { 3:'c', 4:'d'}
