f4b3b8e7f191cdf35c76e69fd95a01ea11375d83
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-05 16:51:28 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -361,7 +361,13 @@ console.log(Array.of(val1, val2, val3));
 
 //object singleton or using constructor
 
-//const facebook = new Object()
-const facebook = {}
+//const facebook = new Object() // singleton object 
+const facebook = {} // non singleton object
+
+console.log(facebook)
+
+facebook.id = '123abs'
+facebook.name = 'Aishwarya'
+facebook.isLoggedin = false
 
 console.log(facebook)
