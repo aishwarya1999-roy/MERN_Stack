2cc702ade9ace93f349d1a8f0721dc123fa77dba
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-05 16:23:22 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -341,13 +341,13 @@ const js_user = {
     isloggin : false
 }
 // Object.freeze(js_user)
-// // console.log(js_user.name);
-// // console.log(js_user["name"]);
-// // console.log(js_user["age"]);
-// // console.log(js_user[myst]);
+// console.log(js_user.name);
+// console.log(js_user["name"]);
+// console.log(js_user["age"]);
+// console.log(js_user[myst]);
 // js_user.City = "howrah"
-
 // console.log(js_user);
+
 
 
 
