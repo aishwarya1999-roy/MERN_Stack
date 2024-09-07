c25f074548f894096fab4e65998a5a94e2461618
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 14:55:05 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/12_Functions.js
+++ JS_Foundation/Basics_2.0/12_Functions.js
@@ -1,1 +1,36 @@
 // console.log("************ Functions *****************")
+
+// function myname(){
+//     console.log("A");
+//     console.log("I");
+//     console.log("S");
+//     console.log("H");
+//     console.log("W");
+//     console.log("A");
+//     console.log("R");
+//     console.log("Y");
+//     console.log("A"); 
+// }
+
+// myname()
+
+
+// function add_number(num1, num2){
+//     console.log(num1+num2);   
+// }
+
+// function add_number(num1, num2){
+//     let res = num1+num2 
+//     return res 
+// }
+// const res = add_number(6,4)
+// console.log("Result : ", res);
+
+function loginmessage(username ){ //default values - username = "Sam"
+    if(!username){ // username === undefined
+        console.log("ERROR!! Please Enter a Username!");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginmessage());
