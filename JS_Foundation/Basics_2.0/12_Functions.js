889e8fbdcddf285fa21d42b667981e20d0aa5771
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 14:55:38 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/12_Functions.js
+++ JS_Foundation/Basics_2.0/12_Functions.js
@@ -1,28 +1,28 @@
-// console.log("************ Functions *****************")
+console.log("************ Functions *****************")
 
-// function myname(){
-//     console.log("A");
-//     console.log("I");
-//     console.log("S");
-//     console.log("H");
-//     console.log("W");
-//     console.log("A");
-//     console.log("R");
-//     console.log("Y");
-//     console.log("A"); 
-// }
-// myname()
+function myname(){
+    console.log("A");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("W");
+    console.log("A");
+    console.log("R");
+    console.log("Y");
+    console.log("A"); 
+}
+myname()
 
-// function add_number(num1, num2){
-//     console.log(num1+num2);   
-// }
+function add_number(num1, num2){
+    console.log(num1+num2);   
+}
 
-// function add_number(num1, num2){
-//     let res = num1+num2 
-//     return res 
-// }
-// const res = add_number(6,4)
-// console.log("Result : ", res);
+function add_number(num1, num2){
+    let res = num1+num2 
+    return res 
+}
+const res = add_number(6,4)
+console.log("Result : ", res);
 
 function loginmessage(username ){ //default values - username = "Sam"
     if(!username){ // username === undefined
