976fda2ff1acd2a0cf661a5283c99c46b34ec899
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 14:43:21 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -452,5 +452,5 @@ console.log(Array.of(val1, val2, val3));
 function add_number(num1, num2){
     console.log(num1+num2);   
 }
-const res = add_number("a",4)
+const res = add_number(6,4)
 console.log("Result : ", res);
