32101ccbe8886f02f11dfad57e2e3cd4d8584a18
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 15:00:57 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -481,6 +481,6 @@ const user = {
 }
 
 function handleobjcet (anyobject){
-    console.log(`Username is ${anyobject.use} and price is ${}`);
+    console.log(`Username is ${anyobject.username} and price is ${}`);
     
 }
