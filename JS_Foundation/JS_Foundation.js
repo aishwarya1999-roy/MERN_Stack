5e1c43be8a2b78dd711ee02f5fa34fbeb96a3115
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 15:06:59 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -494,5 +494,5 @@ console.log(Array.of(val1, val2, val3));
 function reeturnarray(getarray){
     return getarray[0]
 }
-const newarray = [100,200,300]
+// const newarray = [100,200,300]
 console.log(reeturnarray([100,200,300]));
