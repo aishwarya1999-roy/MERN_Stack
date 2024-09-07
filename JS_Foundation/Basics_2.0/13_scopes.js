9f6409eb02b9c77b9ae41dcb58ab434403eb99ed
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 16:56:07 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/13_scopes.js
+++ JS_Foundation/Basics_2.0/13_scopes.js
@@ -12,3 +12,25 @@
 // console.log(b);
 //console.log(c); // here c is printing, as var does not care about scope
 
+// function one(){
+//     const user_name = "Aishwarya from One"
+//     function two(){
+//         const web = "youtube.com"
+//         console.log(user_name);
+//     }
+    
+//     two()
+// }
+// one()
+
+// console.log(addone(2))
+// function addone(num){ //in this case you can call the function whereever you want, before or after
+//     return num+1
+// }
+
+
+// const addtwo = function(num){ // in this case , funtion is acting like initiatizing to an object . so here you have to call it afterwords only
+//     return num+1
+// }
+// console.log(addtwo(5))
+
