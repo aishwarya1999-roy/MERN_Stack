4dc744674fcfca44d1bc397df8812b79ca021b06
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 13:34:32 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -405,4 +405,4 @@ console.log(facebook);
 console.log(Object.keys(facebook));
 console.log(Object.values(facebook));
 console.log(Object.entries(facebook));
-console.log();
+console.log(facebook.);
