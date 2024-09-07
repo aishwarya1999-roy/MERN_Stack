00ffbf04a8b8dc0be22338d408107c139e15e8b9
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 15:16:39 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/13_scopes.js
+++ JS_Foundation/Basics_2.0/13_scopes.js
@@ -1,1 +1,15 @@
+
 // console.log("************ Scopes *****************")
+//global scope, block scope
+//global 
+// let a = 300
+// if (true){ //block 
+//     let a = 20
+//     const b = 10
+//     var c = 30
+// }
+
+// console.log(a);
+// console.log(b);
+//console.log(c); // here c is printing, as var does not care about scope
+
