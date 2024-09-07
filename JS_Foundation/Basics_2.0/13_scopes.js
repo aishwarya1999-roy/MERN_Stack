3223b3ee49c53de19ca41a1d77357e96cf3060d3
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 16:56:09 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/13_scopes.js
+++ JS_Foundation/Basics_2.0/13_scopes.js
@@ -1,36 +1,36 @@
-// console.log("************ Scopes *****************")
-//global scope, block scope
-//global 
-// let a = 300
-// if (true){ //block 
-//     let a = 20
-//     const b = 10
-//     var c = 30
-// }
+console.log("************ Scopes *****************")
+global scope, block scope
+global 
+let a = 300
+if (true){ //block 
+    let a = 20
+    const b = 10
+    var c = 30
+}
 
-// console.log(a);
-// console.log(b);
-//console.log(c); // here c is printing, as var does not care about scope
+console.log(a);
+console.log(b);
+console.log(c); // here c is printing, as var does not care about scope
 
-// function one(){
-//     const user_name = "Aishwarya from One"
-//     function two(){
-//         const web = "youtube.com"
-//         console.log(user_name);
-//     }
+function one(){
+    const user_name = "Aishwarya from One"
+    function two(){
+        const web = "youtube.com"
+        console.log(user_name);
+    }
     
-//     two()
-// }
-// one()
+    two()
+}
+one()
 
-// console.log(addone(2))
-// function addone(num){ //in this case you can call the function whereever you want, before or after
-//     return num+1
-// }
+console.log(addone(2))
+function addone(num){ //in this case you can call the function whereever you want, before or after
+    return num+1
+}
 
 
-// const addtwo = function(num){ // in this case , funtion is acting like initiatizing to an object . so here you have to call it afterwords only
-//     return num+1
-// }
-// console.log(addtwo(5))
+const addtwo = function(num){ // in this case , funtion is acting like initiatizing to an object . so here you have to call it afterwords only
+    return num+1
+}
+console.log(addtwo(5))
 
