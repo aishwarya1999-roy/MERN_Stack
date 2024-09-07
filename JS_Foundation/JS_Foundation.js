04c6ccba2d32b56b6385c042588c81a503cef99f
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 16:55:25 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -528,6 +528,6 @@ function addone(num){ //in this case you can call the function whereever you wan
 }
 
 console.log(addtwo(5))
-const addtwo = function(num){ // in this case , funtion is acting like initiatizing to an object . so 
+const addtwo = function(num){ // in this case , funtion is acting like initiatizing to an object . so here you have to call it afterwords only
     return num+1
 }
