0f25db6fc75c8a740319731cf36128a83f38dc58
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 14:52:38 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -462,7 +462,7 @@ console.log(Array.of(val1, val2, val3));
 
 function loginmessage(username){
     if(username === undefined){
-        console.log("ERROR!! please enter a username");
+        console.log("ERROR!! Please enter a username");
         return
     }
     return `${username} just logged in`
