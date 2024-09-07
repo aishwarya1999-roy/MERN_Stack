174f95605e534fcd59964748e3d89201fac98cfc
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-07 13:58:02 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -408,7 +408,7 @@ console.log(Object.entries(facebook));
 console.log(facebook.hasOwnProperty('isLogged') );
 
 
-const course = {
+const course = {L
     coursename : "Hindi",
     price : '333',
     instructor : "aishwraya"
