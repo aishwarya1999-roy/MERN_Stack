b53715bd58b7d42a334bd6d2ee14bd553fd8a13c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 01:00:20 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -555,7 +555,7 @@ console.log(Array.of(val1, val2, val3));
 // }
 // chai()
 
-const chai = () => { //arrow functions
+const chai = () => { //arrow function
     let username = "Aishwarya"
     console.log(this);
 }
