8a91a65e7cd320953bc2426f9390af41d961a9f3
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 13:30:51 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/17_Conditions.js
+++ JS_Foundation/Basics_2.0/17_Conditions.js
@@ -31,27 +31,27 @@ if (tempu>50 && tempu<60){
 
 // Switch case
 
-// const month = 6
-// switch(month){
-//     case 1:
-//         console.log("Jan");
-//         break;
-//     case 2:
-//         console.log("Feb");
-//         break;
-//     case 3:
-//         console.log("Mar");
-//         break;
-//     case 4:
-//         console.log("Apr");
-//         break;
-//     case 5:
-//         console.log("May");
-//         break;
-//     default:
-//         console.log("Deafult");
-//         break;
-// }
+const month = 6
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Deafult");
+        break;
+}
 
 
 // falsy value
