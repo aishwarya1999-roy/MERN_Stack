b86d6fae88a31b843276eca9bd67564ec7c68fac
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 12:38:57 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -615,7 +615,20 @@ console.log(Array.of(val1, val2, val3));
 
 //IF
 
-const isloggedIn = true
-if (isloggedIn){
+// const isloggedIn = true
+// if (isloggedIn){
+// }
+// == for checking condition, === check condition with type 
+// if (2 === "2"){
+//     console.log("Yes");
+// }
 
-}
+// const temp = 32
+// if (temp<50){
+//     console.log("Less than 40");
+// } else{
+//     console.log("More than 40");
+// }
+
+const balance = 300
+if (balance>200) console.log("Greater than");
