e8d565a1c228d7f994f5f7193390a703ae626678
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 09:47:29 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/15_IIFE.js
+++ JS_Foundation/Basics_2.0/15_IIFE.js
@@ -1,12 +1,12 @@
 // console.log("************ Immediately Invoked Function Expression *****************")
-
-
+ //we use IIFE to avoid global scope pollution
+// (function defination)(execution call)
+// Named IIFE
 (function iffe(){
     console.log("DB CONNECTED"); 
-})(); //we use IIFE to avoid global scope pollution
+})();
 
-// (function defination)(execution call)
-
+// Unamed IIFE
 ((name) => {
     console.log(`DB CONNECTED TWO ${name}`); 
 })("Aishwarya");
