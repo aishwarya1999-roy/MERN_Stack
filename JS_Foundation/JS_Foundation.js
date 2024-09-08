713fdaf761cefc04108eeb89f1c15d583dac0c33
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 12:41:10 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -633,7 +633,7 @@ console.log(Array.of(val1, val2, val3));
 // const balance = 300
 // if (balance>200) console.log("Greater than");
 
-const temp = 1
+const temp = 21
 if (temp>50){
     console.log("More than 40");
 } else if (temp>20) {
