d57da2676f3b6c30f8a504dc5fb458dbefdad694
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 13:16:30 +0530

    Auto-commit

--- JS_Foundation/Control FLow/17_Conditions.js
+++ JS_Foundation/Control FLow/17_Conditions.js
@@ -1,21 +1,57 @@
+
 // console.log("************ Control flow *****************")
 
 //IF
 
-const isloggedIn = true
-if (isloggedIn){
-}
-//== for checking condition, === check condition with type 
-if (2 === "2"){
-    console.log("Yes");
-}
+// const isloggedIn = true
+// if (isloggedIn){
+// }
+// == for checking condition, === check condition with type 
+// if (2 === "2"){
+//     console.log("Yes");
+// }
 
-const temp = 32
-if (temp<50){
-    console.log("Less than 40");
-} else{
-    console.log("More than 40");
-}
+// const temp = 32
+// if (temp<50){
+//     console.log("Less than 40");
+// } else{
+//     console.log("More than 40");
+// }
 
-const balance = 300
-if (balance>200) console.log("Greater than");
+// const balance = 300
+// if (balance>200) console.log("Greater than");
+
+// const temp = 25
+// if (temp>50 && temp<60){
+//     console.log("More than 50 & Less than 60");
+// } else if (temp>20 && temp%5 == 0) {
+//     console.log("More than 20");
+// }else{
+//     console.log("less than 20");
+// }
+
+// Switch case
+
+// const month = 6
+// switch(month){
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     default:
+//         console.log("Deafult");
+//         break;
+// }
+
+
