a705277fa55fad550b494c528851c4c0a7d2dfdd
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 12:44:19 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -636,7 +636,7 @@ console.log(Array.of(val1, val2, val3));
 const temp = 25
 if (temp>50 && temp<60){
     console.log("More than 50 & Less than 60");
-} else if (temp>20 && temp//5 == 0) {
+} else if (temp>20 && temp/5 == 0) {
     console.log("More than 20");
 }else{
     console.log("less than 20");
