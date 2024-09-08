405d9f953ac5d77be240af7efc4a9bf0cc2923be
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 12:39:10 +0530

    Auto-commit

--- JS_Foundation/Control FLow/17_Conditions.js
+++ JS_Foundation/Control FLow/17_Conditions.js
@@ -2,6 +2,20 @@
 
 //IF
 
-?
+const isloggedIn = true
+if (isloggedIn){
+}
+== for checking condition, === check condition with type 
+if (2 === "2"){
+    console.log("Yes");
+}
+
+const temp = 32
+if (temp<50){
+    console.log("Less than 40");
+} else{
+    console.log("More than 40");
+}
+
 const balance = 300
 if (balance>200) console.log("Greater than");
