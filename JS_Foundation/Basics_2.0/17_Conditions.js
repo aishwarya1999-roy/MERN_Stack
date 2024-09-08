77314b02cc8dc43441f7461a5050685f47837d8b
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 13:30:01 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/17_Conditions.js
+++ JS_Foundation/Basics_2.0/17_Conditions.js
@@ -1,78 +1,78 @@
-// console.log("************ Control flow *****************")
+console.log("************ Control flow *****************")
 
-//IF
+IF
 
-// const isloggedIn = true
-// if (isloggedIn){
-// }
-// == for checking condition, === check condition with type 
-// if (2 === "2"){
-//     console.log("Yes");
-// }
+const isloggedIn = true
+if (isloggedIn){
+}
+== for checking condition, === check condition with type 
+if (2 === "2"){
+    console.log("Yes");
+}
 
-// const temp = 32
-// if (temp<50){
-//     console.log("Less than 40");
-// } else{
-//     console.log("More than 40");
-// }
+const temp = 32
+if (temp<50){
+    console.log("Less than 40");
+} else{
+    console.log("More than 40");
+}
 
-// const balance = 300
-// if (balance>200) console.log("Greater than");
+const balance = 300
+if (balance>200) console.log("Greater than");
 
-// const temp = 25
-// if (temp>50 && temp<60){
-//     console.log("More than 50 & Less than 60");
-// } else if (temp>20 && temp%5 == 0) {
-//     console.log("More than 20");
-// }else{
-//     console.log("less than 20");
-// }
+const temp = 25
+if (temp>50 && temp<60){
+    console.log("More than 50 & Less than 60");
+} else if (temp>20 && temp%5 == 0) {
+    console.log("More than 20");
+}else{
+    console.log("less than 20");
+}
 
-// Switch case
+Switch case
 
-// const month = 6
-// switch(month){
-//     case 1:
-//         console.log("Jan");
-//         break;
-//     case 2:
-//         console.log("Feb");
-//         break;
-//     case 3:
-//         console.log("Mar");
-//         break;
-//     case 4:
-//         console.log("Apr");
-//         break;
-//     case 5:
-//         console.log("May");
-//         break;
-//     default:
-//         console.log("Deafult");
-//         break;
-// }
+const month = 6
+switch(month){
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Deafult");
+        break;
+}
 
 
-// falsy value
-// false, 0, -0, BigInt 0n, "", undefined, null, NaN
+falsy value
+false, 0, -0, BigInt 0n, "", undefined, null, NaN
 
-//truthy values
-// "0", 'false'," ", [], {}, funtion(){}, 
+truthy values
+"0", 'false'," ", [], {}, funtion(){}, 
 
-// && , || - logical operator
+&& , || - logical operator
 
-// nullish coalescing operator (??): null undefined
-// let val1;
-// val1 = 5??10
-// val2 = null??10
-// val3 = undefined??15
-// console.log(val1);
-// console.log(val2);
-// console.log(val3);
+nullish coalescing operator (??): null undefined
+let val1;
+val1 = 5??10
+val2 = null??10
+val3 = undefined??15
+console.log(val1);
+console.log(val2);
+console.log(val3);
 
-//Terniary operator
-// condition ? true : false 
+Terniary operator
+condition ? true : false 
 
-// const teaprice = 100
-// teaprice<=80 ? console.log("Less than 80") : console.log("Grater than 80")
+const teaprice = 100
+teaprice<=80 ? console.log("Less than 80") : console.log("Grater than 80")
