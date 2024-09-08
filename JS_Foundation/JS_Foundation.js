d5bf7eb2214cfbd066629b805b213f025aa6b9fc
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 10:03:22 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -597,5 +597,5 @@ console.log(Array.of(val1, val2, val3));
 
 1. gloabl Execution -> this
 2. memory phase -> 
-    val -> undefined
-    val -> undefined
+    val1 -> undefined
+    val2 -> undefined
