09dbb784a386cf5d3ba140c71847c3a263fde46c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 13:30:26 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/17_Conditions.js
+++ JS_Foundation/Basics_2.0/17_Conditions.js
@@ -17,7 +17,17 @@ if (temp<50){
     console.log("More than 40");
 }
 
-?
+const balance = 300
+if (balance>200) console.log("Greater than");
+
+const temp = 25
+if (temp>50 && temp<60){
+    console.log("More than 50 & Less than 60");
+} else if (temp>20 && temp%5 == 0) {
+    console.log("More than 20");
+}else{
+    console.log("less than 20");
+}
 
 // Switch case
 
