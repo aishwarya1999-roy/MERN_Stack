c848cd151a099d9029e485512f94d4d38390a468
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-08 13:31:59 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -675,13 +675,13 @@ switch(month){
 // && , || - logical operator
 
 // nullish coalescing operator (??): null undefined
-let val1;
-val1 = 5??10
-val2 = null??10
-val3 = undefined??15
-console.log(val1);
-console.log(val2);
-console.log(val3);
+// let val1;
+// val1 = 5??10
+// val2 = null??10
+// val3 = undefined??15
+// console.log(val1);
+// console.log(val2);
+// console.log(val3);
 
 //Terniary operator
 // condition ? true : false 
