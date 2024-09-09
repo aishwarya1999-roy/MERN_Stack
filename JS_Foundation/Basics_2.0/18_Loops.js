3240b81f85948273ffa84628249e485d5df8168c
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-09 18:08:23 +0530

    Auto-commit

---
+++ JS_Foundation/Basics_2.0/18_Loops.js
@@ -0,0 +1,26 @@
+
+// console.log("************ Loops *****************")
+
+
+// for loop
+// ara = [1,2,3,5,6]
+// for (let i = 0; i <= ara.length; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// while
+// let i = 0
+// while (i<=10){
+//     console.log(`I = ${i}`);
+//     i+=2
+// }
+
+// Do-while
+// let i = 13
+// do {
+//     console.log(`I = ${i}`);
+//     i++
+// }while (i<=10);
+
+// for of
