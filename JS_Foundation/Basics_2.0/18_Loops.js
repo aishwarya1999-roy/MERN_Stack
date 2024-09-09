4480ef2995f7d4c05c005e7694c2186473496fb0
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-09 18:26:19 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/18_Loops.js
+++ JS_Foundation/Basics_2.0/18_Loops.js
@@ -71,6 +71,6 @@ console.log("************ Loops *****************")
 
 const coding = ['JS', "CPP", "Python", "ruby"]
 coding.forEach(element => {
-    console.log();
+    console.log(element);
     
 });
