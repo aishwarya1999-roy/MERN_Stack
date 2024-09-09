9f675a8783845b0affe2905268f60ed02aa8735f
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-09 18:23:13 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/18_Loops.js
+++ JS_Foundation/Basics_2.0/18_Loops.js
@@ -61,8 +61,4 @@ console.log("************ Loops *****************")
 
 const programming = ['JS', "CPP", "Python", "ruby"]
 for (const key in object) {
-    if (Object.prototype.hasOwnProperty.call(object, key)) {
-        const element = object[key];
-        
-    }
 }
