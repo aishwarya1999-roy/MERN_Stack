014231fe86b00b5f1163144df1aacacfd2844f82
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-09 18:17:25 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/18_Loops.js
+++ JS_Foundation/Basics_2.0/18_Loops.js
@@ -43,6 +43,6 @@ newmap.set("US", "United States")
 newmap.set("FR", "France")
 newmap.set("IN", "India") //will not added
 //console.log(newmap);
-for (const [key] of newmap) {
-    console.log(key); 
-}
+// for (const [key] of newmap) {
+//     console.log(key); 
+// }
