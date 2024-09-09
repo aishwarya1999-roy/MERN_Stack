5754fc2f8b23560369655fe1629f79886093b2e5
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-09 17:24:46 +0530

    Auto-commit

--- JS_Foundation/JS_Foundation.js
+++ JS_Foundation/JS_Foundation.js
@@ -695,8 +695,8 @@ console.log(Array.of(val1, val2, val3));
 
 // for loop
 ara = [1,2,3,5,6]
-for (let i = 0; i <= ara.length; i++) {
-    const element = i;
+for (let in = 0; in <= ara.length; in++) {
+    const element = in;
     console.log(element);
 }
 
