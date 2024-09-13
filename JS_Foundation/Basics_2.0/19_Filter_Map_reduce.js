9191d9f386cd86c3a750c4f63868c8535d8c1dd1
Aishwarya Roy <aishwarya99roy@gmail.com>
2024-09-13 18:25:23 +0530

    Auto-commit

--- JS_Foundation/Basics_2.0/19_Filter_Map_reduce.js
+++ JS_Foundation/Basics_2.0/19_Filter_Map_reduce.js
@@ -1,24 +1,24 @@
-const nums = [1,4,5,2,8]
-//filter
+// const nums = [1,4,5,2,8]
+// //filter
 
-const new_nums = nums.filter( (num) => num>3)
-console.log(new_nums);
-
-
-//map
-
-const new_nums_map = nums.map((num) => (num+10))
-console.log(new_nums_map);
+// const new_nums = nums.filter( (num) => num>3)
+// console.log(new_nums);
 
 
-//reduce
+// //map
 
-const nums_reduce = nums.reduce(function(acc,cur){
-    console.log(`acc : ${acc}, curent value ${cur}`);
+// const new_nums_map = nums.map((num) => (num+10))
+// console.log(new_nums_map);
+
+
+// //reduce
+
+// const nums_reduce = nums.reduce(function(acc,cur){
+//     console.log(`acc : ${acc}, curent value ${cur}`);
     
-    return acc+cur
-}, 0)
-console.log(nums_reduce);
+//     return acc+cur
+// }, 0)
+// console.log(nums_reduce);
 
-const nums_reduce_arrow = nums.reduce((acc,cur) => (acc+cur), 0)
-console.log(nums_reduce_arrow);
+// const nums_reduce_arrow = nums.reduce((acc,cur) => (acc+cur), 0)
+// console.log(nums_reduce_arrow);
